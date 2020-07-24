a8d3dec3c48daf070e4b7a5e42d3a688a77ebab7
Ebubechukwu <dimobiblaise@gmail.com>
2020-07-25 00:31:46 +0300

    Initial Commit

---
+++ WhatFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatFood
+//
+//  Created by Ebubechukwu Dimobi on 25.07.2020.
+//  Copyright © 2020 blazeapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
