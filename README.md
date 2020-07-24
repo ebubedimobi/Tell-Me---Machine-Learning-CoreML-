78ba8e6c886948aaea249ae2126b7062f2701d0d
Ebubechukwu <dimobiblaise@gmail.com>
2020-07-25 02:45:58 +0300

    First Version Ready

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tell Me - CoreML
+
+Using Machine Learning with CoreML, get answers to any information you need on what an item is. All data available offline
+
+# Screenshots
+
+![](Documentation/iphone11_1.png)
+![](Documentation/iphone11_2.png)
+![](Documentation/iphone11_3.png)
+
--- WhatFood/ViewController.swift
+++ WhatFood/ViewController.swift
@@ -7,14 +7,110 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var responseLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        responseLabel.isHidden = true
+        answerLabel.text = nil
+        answerLabel.isHidden = true
+        
+        imagePicker.delegate = self
+        //imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = true
     }
-
-
+    
+    
+    
+    @IBAction func camerTapped(_ sender: UIBarButtonItem) {
+        
+        let actionsheet = UIAlertController(title: "Photo Source", message: "Choose A Source", preferredStyle: .actionSheet)
+        
+        actionsheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction)in
+            if UIImagePickerController.isSourceTypeAvailable(.camera){
+                self.imagePicker.sourceType = .camera
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }else
+            {
+                print("Camera is Not Available")
+            }
+        }))
+        actionsheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction)in
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        actionsheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionsheet,animated: true, completion: nil)
+        
+        present(imagePicker, animated: true)
+    }
+    
+    @IBAction func refreshButtonClicked(_ sender: UIBarButtonItem) {
+        
+        imageView.image = nil
+        responseLabel.isHidden = true
+        answerLabel.text = nil
+        answerLabel.isHidden = true
+        
+    }
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedimage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+              imageView.image = userPickedimage
+              
+            guard let ciimage = CIImage(image: userPickedimage) else{
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(with: ciimage)
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func detect(with image: CIImage){
+        
+        do{
+            let model = try VNCoreMLModel(for: Inceptionv3().model)
+            
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                guard let results = request.results as? [VNClassificationObservation] else {
+                    fatalError("Could not process request")
+                }
+                
+                print(results[0].identifier)
+                self.answerLabel.isHidden = false
+                self.answerLabel.text = results[0].identifier
+                self.responseLabel.isHidden = false
+                
+            }
+            
+            let handler = VNImageRequestHandler(ciImage: image)
+            try handler.perform([request])
+        }catch{
+            print("error while processing image\(error)")
+        }
+        
+        
+        
+        
+    }
+    
+}
